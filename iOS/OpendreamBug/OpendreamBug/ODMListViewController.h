e606457b3c50b3cb43e3fb86e40daf9149ba27b1
plloyniie <PlloYNiiE@gmail.com>
2012-09-10 18:39:50 +0700

    create first scene (tableviewController)

---
+++ iOS/OpendreamBug/OpendreamBug/ODMListViewController.h
@@ -0,0 +1,14 @@
+//
+//  ODMListViewController.h
+//  OpendreamBug
+//
+//  Created by Pirapa on 9/10/12.
+//  Copyright (c) 2012 opendream. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ODMListViewController : UITableViewController
+@property (strong, nonatomic) IBOutlet UIToolbar *toolBar;
+
+@end
---
+++ iOS/OpendreamBug/OpendreamBug/ODMListViewController.m
@@ -0,0 +1,115 @@
+//
+//  ODMListViewController.m
+//  OpendreamBug
+//
+//  Created by Pirapa on 9/10/12.
+//  Copyright (c) 2012 opendream. All rights reserved.
+//
+
+#import "ODMListViewController.h"
+
+#define kSceenSize self.parentViewController.view.frame.size
+#define kToolBarSize toolBar.frame.size
+
+@interface ODMListViewController ()
+
+@end
+
+@implementation ODMListViewController
+@synthesize toolBar;
+
+- (id)initWithStyle:(UITableViewStyle)style
+{
+    self = [super initWithStyle:style];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+#pragma mark - View's Life Cycle
+
+-(void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    [self.parentViewController.view addSubview:toolBar];
+    toolBar.frame = CGRectMake(0, kSceenSize.height - kToolBarSize.height, kToolBarSize.width, kToolBarSize.height);
+}
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+- (void)viewDidUnload
+{
+    [self setToolBar:nil];
+    [super viewDidUnload];
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 2;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"BugCell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    
+    // Configure the cell...
+    
+    return cell;
+}
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    }   
+    else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+
+
+
+#pragma mark - Table view delegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Navigation logic may go here. Create and push another view controller.
+    /*
+     <#DetailViewController#> *detailViewController = [[<#DetailViewController#> alloc] initWithNibName:@"<#Nib name#>" bundle:nil];
+     // ...
+     // Pass the selected object to the new view controller.
+     [self.navigationController pushViewController:detailViewController animated:YES];
+     */
+}
+
+@end
