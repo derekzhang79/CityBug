5fc74f245726a943096cedbd93952d43376b8edc
Chongsawad Saiaram <chongsawad252@gmail.com>
2012-09-10 18:06:39 +0700

    add category model & test

--- iOS/OpendreamBug/OpendreamBug/Model/ODMEntry.h
+++ iOS/OpendreamBug/OpendreamBug/Model/ODMEntry.h
@@ -19,6 +19,7 @@
 @property (nonatomic, retain) NSNumber * latitude;
 @property (nonatomic, retain) NSNumber * longitude;
 @property (nonatomic, retain) NSString * note;
+@property (nonatomic, retain) NSDate * lastUpdated;
 @property (nonatomic, retain) ODMCategory *category;
 
 @end
--- iOS/OpendreamBug/OpendreamBug/Model/ODMEntry.m
+++ iOS/OpendreamBug/OpendreamBug/Model/ODMEntry.m
@@ -9,12 +9,6 @@
 #import "ODMEntry.h"
 #import "ODMCategory.h"
 
-#define NAME_INVALID_TITLE @"Name does not valid."
-#define NAME_INVALID_STRING @"Please use only letters (a-z, A-Z, ก-ฮ), numbers, periods or whitespace"
-#define NAME_LENGTH_INVALID @"Name should be less than 30 characters"
-
-#define VALIDATION_USERNAME_REGEXR @"^[a-zA-Z0-9ก-ฮเ-ไ]{1}[a-zA-Z0-9ก-๙]{2}[a-zA-Z0-9ก-๙ _]*"
-
 @implementation ODMEntry
 
 @dynamic title;
@@ -23,7 +17,9 @@
 @dynamic latitude;
 @dynamic longitude;
 @dynamic note;
+@dynamic lastUpdated;
 @dynamic category;
+
 
 /*
  * Validate title field
@@ -37,12 +33,23 @@
 
 - (BOOL)validateValue:(__autoreleasing id *)value forKey:(NSString *)key error:(NSError *__autoreleasing *)error
 {
+    /*
+     * Validate Title field
+     */
     if ([key isEqualToString:@"title"]) {
-        if (self.title.length > MAXIMUM_CONTACT_NAME_LENGTH) {
-            *error = [NSError errorWithDomain:NAME_LENGTH_INVALID code:1002 userInfo:[NSDictionary dictionaryWithObject:self forKey:@"ODMEntry"]];
+        if (self.title.length > MAXIMUM_ENTRY_LENGTH) {
+            *error = [NSError errorWithDomain:NAME_LENGTH_INVALID code:1002 userInfo:[NSDictionary dictionaryWithObject:self forKey:NSStringFromClass([self class])]];
             return NO;
         } else if ([self validateTitleField:self.title] == NO) {
-            *error = [NSError errorWithDomain:NAME_INVALID_STRING code:1003 userInfo:[NSDictionary dictionaryWithObject:self forKey:@"ODMEntry"]];
+            *error = [NSError errorWithDomain:NAME_INVALID_STRING code:1003 userInfo:[NSDictionary dictionaryWithObject:self forKey:NSStringFromClass([self class])]];
+            return NO;
+        }
+    /*
+     * Validate Note field
+     */
+    } else if ([key isEqualToString:@"note"]) {
+        if (self.note.length > MAXIMUM_NOTE_LENGTH) {
+            *error = [NSError errorWithDomain:LONG_LENGTH_STRING_ERROR_DOMAIN code:1004 userInfo:[NSDictionary dictionaryWithObject:self forKey:NSStringFromClass([self class])]];
             return NO;
         }
     }
