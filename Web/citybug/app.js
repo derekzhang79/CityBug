86d357bb7028c0d1f390c7a20f83191084d2c21b
Sarocha Sothornprapakorn <tua.sarocha@gmail.com>
2012-09-11 16:37:00 +0700

    add socket.io lib

--- Web/citybug/app.js
+++ Web/citybug/app.js
@@ -3,7 +3,7 @@ var express = require('express'),
     routes = require('./routes');
 
 var app = module.exports = express.createServer();
-
+var io = require('socket.io').listen(app);
 
 // Configuration
 require('./configuration')(app, express);
@@ -15,3 +15,11 @@ app.post('/', routes.index_post);
 // Open App socket
 app.listen(3003);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+
+io.sockets.on('connection', function (socket) {
+	socket.emit('news', { hello: 'world' });
+    socket.on('message', function (msg) {
+        console.log('Message Received: ', msg);
+        socket.broadcast.emit('message', msg);
+    });
+});
