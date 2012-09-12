4f7efcb218e9ecbd299d9856f081b4122e894acc
nut <nut@opendream.co.th>
2012-09-12 10:55:25 +0700

    config body upload

--- Web/citybug/configuration.js
+++ Web/citybug/configuration.js
@@ -2,7 +2,7 @@ module.exports = function(app, express){
 	app.configure(function(){
 	    app.set('views', __dirname + '/views');
 	    app.set('view engine', 'jade');
-	    app.use(express.bodyParser());
+	    app.use(express.bodyParser({uploadDir:'./uploads'}));
 	    app.use(express.methodOverride());
 	    app.use(app.router);
 	    app.use(express.static(__dirname + '/public'));
