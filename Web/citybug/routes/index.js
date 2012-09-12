b7607492855f82cd7b3ac38b0205f1131fa20f6c
nut <nut@opendream.co.th>
2012-09-12 10:19:03 +0700

    add upload file, show picture

--- Web/citybug/routes/index.js
+++ Web/citybug/routes/index.js
@@ -19,10 +19,40 @@ exports.index = function(req, res){
 };
 
 exports.index_post = function(req, res){
+
+    var fs = require('fs');
+    // get the temporary location of the file
+    var tmp_path = req.files.thumbnail_image.path;
+    // set where the file should actually exists - in this case it is in the "images" directory
+    var target_path = './public/images/' + req.files.thumbnail_image.name;
+    // move the file from the temporary location to the intended location
+    fs.rename(tmp_path, target_path, function(err) {
+        if (err) throw err;
+        // delete the temporary file, so that the explicitly set temporary upload dir does not get filled with unwanted files
+        fs.unlink(tmp_path, function() {
+            if (err) throw err;
+            console.log('File uploaded to: ' + target_path + ' - ' + req.files.thumbnail_image.size + ' bytes');
+        });
+    });
+
+    var tmp_path = req.files.full_image.path;
+    // set where the file should actually exists - in this case it is in the "images" directory
+    var target_path = './public/images/' + req.files.full_image.name;
+    // move the file from the temporary location to the intended location
+    fs.rename(tmp_path, target_path, function(err) {
+        if (err) throw err;
+        // delete the temporary file, so that the explicitly set temporary upload dir does not get filled with unwanted files
+        fs.unlink(tmp_path, function() {
+            if (err) throw err;
+            console.log('File uploaded to: ' + target_path + ' - ' + req.files.full_image.size + ' bytes');
+        });
+    });
+
+
     entry = new entryModel();
     entry.title = req.body.title;
-    entry.thumbnail_image = req.body.thumbnail_image;
-    entry.full_image = req.body.full_image;
+    entry.thumbnail_image = req.files.thumbnail_image.name;
+    entry.full_image = req.files.full_image.name;
     entry.latitude = req.body.latitude;
     entry.longitude = req.body.longitude;
     entry.note = req.body.note;
