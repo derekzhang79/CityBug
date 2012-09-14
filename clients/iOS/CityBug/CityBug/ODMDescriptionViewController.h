6323cb84d7c7e0decb4270b1706634653d05c950
Chongsawad Saiaram <chongsawad252@gmail.com>
2012-09-14 16:25:58 +0700

    fix null location label

---
+++ clients/iOS/CityBug/CityBug/ODMDescriptionViewController.h
@@ -0,0 +1,22 @@
+//
+//  ODMDescriptionViewController.h
+//  OpendreamBug
+//
+//  Created by Pirapa on 9/11/12.
+//  Copyright (c) 2012 opendream. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "ODMEntry.h"
+
+@interface ODMDescriptionViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UIImageView *bugImageView;
+@property (weak, nonatomic) IBOutlet UILabel *locationLabel;
+@property (weak, nonatomic) IBOutlet UILabel *catergoryLabel;
+@property (weak, nonatomic) IBOutlet UITextView *descTextView;
+@property (weak, nonatomic) NSDictionary *entry;
+
+- (IBAction)doneButtonTapped:(id)sender;
+
+@end
---
+++ clients/iOS/CityBug/CityBug/ODMDescriptionViewController.m
@@ -0,0 +1,57 @@
+//
+//  ODMDescriptionViewController.m
+//  OpendreamBug
+//
+//  Created by Pirapa on 9/11/12.
+//  Copyright (c) 2012 opendream. All rights reserved.
+//
+
+#import "ODMDescriptionViewController.h"
+#import "UIImageView+AFNetworking.h"
+
+@implementation ODMDescriptionViewController
+
+#pragma mark -
+
+- (void)reloadData
+{
+    double latitude = [[self.entry objectForKey:@"latitude"] doubleValue];
+    double longitude = [[self.entry objectForKey:@"longitude"] doubleValue];
+    NSString *location;
+    
+    if (latitude > 0 && longitude > 0) {
+        location = [NSString stringWithFormat:@"%1.3f, %1.3f", latitude, longitude];
+    } else {
+        location = @"";
+    }
+    
+    
+    NSString *imagePath = [BASE_URL stringByAppendingString:[self.entry objectForKey:@"full_image"]];
+    [self.bugImageView setImageWithURL:[NSURL URLWithString:imagePath] placeholderImage:[UIImage imageNamed:@"process"]];
+    self.descTextView.text = [self.entry objectForKey:@"title"];
+    self.locationLabel.text = location;
+    self.catergoryLabel.text = [self.entry objectForKey:@"categories"];
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    [self reloadData];
+}
+
+- (void)viewDidUnload
+{
+    [self setBugImageView:nil];
+    [self setLocationLabel:nil];
+    [self setCatergoryLabel:nil];
+    [self setDescTextView:nil];
+    [super viewDidUnload];
+}
+
+- (IBAction)doneButtonTapped:(id)sender
+{
+    [self.navigationController popViewControllerAnimated:YES];
+}
+
+@end
