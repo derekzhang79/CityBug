655ff8a969348156e2b02d888388984e106e6788
nut <nut@opendream.co.th>
2012-09-19 11:29:43 +0700

    add new model, not include comment, SubscriptionSchema, imim and not fix picture path

--- server/citybug/routes.js
+++ server/citybug/routes.js
@@ -6,9 +6,9 @@ module.exports = function(app, express){
 
 	app.get('/add', api.add);
 
-	app.get('/api/entries', api.entries);
+	app.get('/api/reports', api.reports);
 	// app.post('/api/entries', routes.index_post);
-	app.post('/api/entries', api.entry_post);
+	app.post('/api/reports', api.report_post);
 	//app.post('/api/entries', api.entries_post);
-	app.get('/api/entries/*', api.entry);
+	app.get('/api/reports/*', api.report);
 };
