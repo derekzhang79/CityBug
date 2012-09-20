a9bd8f4b54cd8c8036cfc99a56e95090c29e1527
nut <nut@opendream.co.th>
2012-09-20 16:04:53 +0700

    add mockupdata at index

--- server/citybug/controllers/index.js
+++ server/citybug/controllers/index.js
@@ -11,6 +11,75 @@ var model = service.useModel('model');
 
 // Home page => registration form
 exports.index = function(req, res){
+
+	
+    //add mockup user 
+    model.User.find({} , function(err,allUser) { 
+        if (err || allUser.length < 1) {
+            var user = new model.User();
+            user.username = 'admin';
+            user.password = '1234';
+            user.email = '123@ggg.com';
+            user.created_at = new Date();
+            user.last_modified = new Date();
+            user.save(function (err){
+                if (err) {
+                    console.log(err);
+                    // do something
+                } else {
+                    console.log('user' + user);
+                }
+            }); 
+        }
+    });
+
+    // add Mock up category
+    model.User.find({} , function(err,allUser) { 
+        if (err || allUser.length < 1) {
+            var cat1 = new model.Category();
+            cat1.title = 'cat1';
+            cat1.last_modified = new Date();
+            cat1.created_at = new Date();
+            var cat2 = new model.Category();
+            cat2.title = 'cat2';
+            cat2.last_modified = new Date();
+            cat2.created_at = new Date();
+            cat1.save(function (err){
+                if (err) {
+                    console.log(err);
+                    // do something
+                } else {
+                    console.log('cat1' + cat1);
+                }
+            }); 
+            cat2.save(function (err){
+                if (err) {
+                    console.log(err);
+                    // do something
+                } else {
+                    console.log('cat2' + cat2);
+                }
+            }); 
+        }
+    });
+    
+    // // add mockup place
+    // var place = new model.Place();
+    // place.title = 'สวนดอกจ้า';
+    // place.lat = 12.34;
+    // place.lng = 45.67;
+    // place.created_at = new Date();
+    // place.last_modified = new Date();
+    // place.save(function (err){
+    //     if (err) {
+    //         console.log(err);
+    //         // do something
+    //     } else {
+    //         console.log('place' + place);
+    //     }
+    // }); 
+
+
     var query =  model.Report.find({});
 
     query.exec(function (err, docs) {
