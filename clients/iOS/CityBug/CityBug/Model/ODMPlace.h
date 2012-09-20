ff117fce9cfa32f8c931453d42be129f6ab8fdcc
Chongsawad Saiaram <chongsawad252@gmail.com>
2012-09-20 17:42:12 +0700

    Add place list and search bar

---
+++ clients/iOS/CityBug/CityBug/Model/ODMPlace.h
@@ -0,0 +1,17 @@
+//
+//  ODMPlace.h
+//  CityBug
+//
+//  Created by InICe on 20/9/2555.
+//  Copyright (c) พ.ศ. 2555 opendream. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ODMPlace : NSObject
+
+@property (nonatomic, strong) NSString *title;
+@property (nonatomic, strong) NSNumber *latitude;
+@property (nonatomic, strong) NSNumber *longitude;
+
+@end
