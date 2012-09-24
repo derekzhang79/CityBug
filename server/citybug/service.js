7053123ce4474e3fa990fbe722793c93256b5513
Sarocha Sothornprapakorn <tua.sarocha@gmail.com>
2012-09-24 11:20:54 +0700

    fixed bug  mockup category, move calculate distance function to service.js

--- server/citybug/service.js
+++ server/citybug/service.js
@@ -16,3 +16,20 @@ module.exports.useModel = function (modelName) {
 module.exports.useModule = function (moduleName) {
     return require("./modules/" + moduleName);
 };
+
+module.exports.distanceCalculate = function (lat1, lon1, lat2, lon2) {
+	var radlat1 = Math.PI * lat1/180;
+	var radlat2 = Math.PI * lat2/180;
+	var radlon1 = Math.PI * lon1/180;
+	var radlon2 = Math.PI * lon2/180;
+	var theta = lon1-lon2;
+	var radtheta = Math.PI * theta/180;
+	var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+	dist = Math.acos(dist);
+	dist = dist * 180/Math.PI;
+	dist = dist * 60 * 1.1515;
+	dist = dist * 1.609344;  //kilometre
+
+	//console.log("distance from "+lat1 +" "+ lon1+" "+ lat2 +" "+ lon2 + " = " + dist + "km");
+	return dist;
+};
