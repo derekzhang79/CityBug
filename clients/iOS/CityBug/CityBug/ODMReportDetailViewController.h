0dea385955e5d797be2147a693b98d0095f2a705
Chongsawad Saiaram <chongsawad252@gmail.com>
2012-09-26 17:35:08 +0700

    Add comment and sign-in view to storyboard

--- clients/iOS/CityBug/CityBug/ODMReportDetailViewController.h
+++ clients/iOS/CityBug/CityBug/ODMReportDetailViewController.h
@@ -9,13 +9,34 @@
 #import "ODMReportCommentViewController.h"
 #import "ODMReport.h"
 
-@interface ODMReportDetailViewController : UIViewController<UITextFieldDelegate, ODMReportCommentDelegate>
+@interface ODMReportDetailViewController : UIViewController <UITextFieldDelegate, ODMReportCommentDelegate, UIScrollViewDelegate, UITableViewDataSource, UITableViewDelegate> {
+    
+    UITableView *_tableView;
+    
+    ODMReport *_report;
+}
 
-@property (weak, nonatomic) ODMReport *entry;
-@property (weak, nonatomic) IBOutlet UIImageView *reportImageView;
-@property (weak, nonatomic) IBOutlet UILabel *titleLabel;
-@property (weak, nonatomic) IBOutlet UILabel *locationLabel;
-@property (weak, nonatomic) IBOutlet UILabel *CommentLabel;
+/*
+ * Information including comments
+ */
+@property (nonatomic, strong) ODMReport *report;
+
+/*
+ * View
+ */
+@property (nonatomic, strong) IBOutlet UITableView *tableView;
+
+/*
+ * Accessory
+ */
+@property (weak, nonatomic) IBOutlet UIView *commentFormView;
+@property (weak, nonatomic) IBOutlet UIImageView *reportImageView, *avatarImageView;
+@property (weak, nonatomic) IBOutlet UILabel *titleLabel, *locationLabel, *commentLabel, *iminLabel, *userLabel, *lastModifiedLabel, *noteLabel;
+@property (weak, nonatomic) IBOutlet UITextField *commentTextField;
+
+/*
+ * Add comment
+ */
 - (IBAction)addCommentButtonTapped:(id)sender;
 
 @end
--- clients/iOS/CityBug/CityBug/ODMReportDetailViewController.m
+++ clients/iOS/CityBug/CityBug/ODMReportDetailViewController.m
@@ -9,63 +9,31 @@
 #import "ODMReportDetailViewController.h"
 
 #import "UIImageView+WebCache.h"
+
+#import "NSDate+HumanizedTime.h"
+
 #import "ODMDataManager.h"
 
-@implementation ODMReportDetailViewController {
-    int numberOfComment;
-    NSMutableDictionary *commentDict;
-    NSMutableArray *commentArray;
-}
-
-@synthesize reportImageView;
-@synthesize titleLabel;
-@synthesize locationLabel;
-@synthesize CommentLabel;
-
-
+@implementation ODMReportDetailViewController
 
 - (void)reloadData
 {
-    double latitude = [[self.entry latitude] doubleValue];
-    double longitude = [[self.entry longitude] doubleValue];
-    NSString *location;
     
-    if (latitude > 0 && longitude > 0) {
-        location = [NSString stringWithFormat:@"%1.3f, %1.3f", latitude, longitude];
-        NSLog(@"location %@", location);
-    } else {
-        location = @"";
-    }
-
-    self.titleLabel.text = [self.entry title];
-    self.locationLabel.text = location;
-    NSURL *reportURL = [NSURL URLWithString:[NSString stringWithFormat:@"%@%@", BASE_URL, [self.entry fullImage]]];
-    ODMLog(@"report URL %@",[reportURL absoluteString]);
-    [self.reportImageView setImageWithURL:reportURL placeholderImage:[UIImage imageNamed:@"bugs.jpeg"] options:SDWebImageCacheMemoryOnly];
-
-    
-
 }
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    numberOfComment = 0;
-    [self reloadData];
-    ODMReportCommentViewController *reportComment = [[ODMReportCommentViewController alloc] init];
-    reportComment.delegate = self;
-    commentDict = [[NSMutableDictionary alloc] init];
-    commentArray = [[NSMutableArray alloc] init];
+    
+//    ODMReportCommentViewController *reportComment = [[ODMReportCommentViewController alloc] init];
+//    reportComment.delegate = self;
 }
 
-- (void)viewDidUnload
+- (BOOL)resignFirstResponder
 {
-
-    [self setLocationLabel:nil];
-    [self setReportImageView:nil];
-    [self setTitleLabel:nil];
-    [self setCommentLabel:nil];
-    [super viewDidUnload];
+    [self.commentTextField resignFirstResponder];
+    
+    return [super resignFirstResponder];
 }
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
@@ -73,18 +41,25 @@
     return (interfaceOrientation == UIInterfaceOrientationPortrait);
 }
 
+#pragma mark - Datasource
+
+- (void)setReport:(ODMReport *)report
+{
+    _report = report;
+}
+
 #pragma mark - FormField Delegate
 
 - (void)updateComment:(NSString *)comment
 {
-    NSLog(@"Comment %@", comment);
-
-    self.CommentLabel.text = comment;
+    self.commentLabel.text = comment;
+ 
     ODMComment *commentObject = [[ODMComment alloc] init];
-    [commentObject setText:comment];
-    [commentObject setReportID:self.entry.uid];
-    [[ODMDataManager sharedInstance] postComment:commentObject];
     
+    [commentObject setText:comment];
+    [commentObject setReportID:self.report.uid];
+    
+    [[ODMDataManager sharedInstance] postComment:commentObject];
 }
 
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
@@ -94,7 +69,48 @@
         commentVC.delegate = self;
     }
 }
-- (IBAction)addCommentButtonTapped:(id)sender {
+
+- (IBAction)addCommentButtonTapped:(id)sender
+{
     
 }
+
+#pragma mark - UIScrollView
+
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView
+{
+    [self resignFirstResponder];
+}
+
+#pragma mark - UITableView
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 5;
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    // [self.report.comments count]
+    return 1;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *cellIdentifier = @"CommentCellIdentifier";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+    
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:cellIdentifier];
+    }
+    
+//    ODMComment *comment = [self.report.comments objectAtIndex:indexPath.row];
+//    cell.textLabel.text = [self.report.user username];
+//    cell.detailTextLabel.text = [comment text];
+    
+    cell.textLabel.text = @"admin";
+    cell.detailTextLabel.text = @"qqqqwwwweeeerrr";
+    return cell;
+}
+
 @end
