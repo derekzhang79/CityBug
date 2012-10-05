1e4f2e831ea76e10325860f4c3b19af3efdbdf2a
nut <nut@opendream.co.th>
2012-10-05 13:36:14 +0700

    remove socket.io

--- server/citybug/app.js
+++ server/citybug/app.js
@@ -14,11 +14,3 @@ require('./routes')(app, express);
 // Open App socket
 app.listen(3003);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
-
-var io = sio.listen(app);
-
-io.sockets.on('connection', function (socket) {
-	socket.on('user message', function (msg) {
-    	socket.broadcast.emit('user message', msg);
-  	});
-});
