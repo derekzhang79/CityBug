504a39a9aed5f1760803eb0c4bd9e0c87f8f8629
Sarocha Sothornprapakorn <tua.sarocha@gmail.com>
2012-10-09 15:56:50 +0700

    add Alert when no internet connection

--- clients/iOS/CityBug/CityBug/ODMAppDelegate.m
+++ clients/iOS/CityBug/CityBug/ODMAppDelegate.m
@@ -17,8 +17,27 @@
     RKLogConfigureByName("RestKit/Network*", RKLogLevelWarning);
     RKLogConfigureByName("RestKit/ObjectMapping", RKLogLevelWarning);
     
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(reachabilityStatusChanged:)
+                                                 name:RKReachabilityDidChangeNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(reachabilityStatusChanged:)
+                                                 name:RKReachabilityWasDeterminedNotification object:nil];
+    
     return YES;
 }
 
+- (void)reachabilityStatusChanged:(NSNotification *)notification
+{
+    NSLog(@">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+    if ([[[RKClient sharedClient] reachabilityObserver] isReachabilityDetermined] && [[RKClient sharedClient] isNetworkReachable]) {
+        NSLog(@"Internet reachable");
+    } else {
+        NSLog(@"Internet not reachable");
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"No internet connection!" message:@"Please connect internet to use citybug" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles: nil];
+        [alert show];
+    }
+    NSLog(@">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+}
 
 @end
