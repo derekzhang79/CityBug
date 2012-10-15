5d21955f16509e5829b38f0ea629394c5aece477
nut <nut@opendream.co.th>
2012-10-15 16:12:13 +0700

    update web index page

--- server/citybug/app.js
+++ server/citybug/app.js
@@ -4,8 +4,24 @@ var express = require('express'),
 	passport = require('passport'),
 	flash = require('connect-flash'),
 	util = require('util');
-var app = module.exports = express.createServer();
+const crypto = require('crypto'),
+      fs = require("fs");
+// ssl
+var privateKey = fs.readFileSync('privatekey.pem').toString();
+var certificate = fs.readFileSync('certificate.pem').toString();
 
+var credentials = crypto.createCredentials({key: privateKey, cert: certificate});
+
+
+var hskey = fs.readFileSync('privatekey.pem');
+var hscert = fs.readFileSync('certificate.pem')
+
+var options = {
+    key: hskey,
+    cert: hscert
+};
+
+var app = module.exports = express.createServer();
 // Configuration
 require('./configuration')(app, express, passport, flash);
 require('./routes')(app, express);
